75a1d05077a0020319ee28c30ac8e6880ca6dc42
momenalfadel3-star <momenalfadel3@gmail.com>
2026-01-17 00:37:14 +0200

    Upload via Bot

---
+++ ui/flutter/lib/api/model/options.dart
@@ -0,0 +1,45 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'options.g.dart';
+
+@JsonSerializable(explicitToJson: true)
+class Options {
+  String name;
+  String path;
+  List<int> selectFiles;
+  Object? extra;
+
+  Options({
+    this.name = '',
+    this.path = '',
+    this.selectFiles = const [],
+    this.extra,
+  });
+
+  factory Options.fromJson(Map<String, dynamic> json) =>
+      _$OptionsFromJson(json);
+
+  Map<String, dynamic> toJson() => _$OptionsToJson(this);
+}
+
+@JsonSerializable()
+class OptsExtraHttp {
+  int connections;
+  bool autoTorrent;
+  bool autoExtract;
+  String archivePassword;
+  bool deleteAfterExtract;
+
+  OptsExtraHttp({
+    this.connections = 0,
+    this.autoTorrent = false,
+    this.autoExtract = false,
+    this.archivePassword = '',
+    this.deleteAfterExtract = false,
+  });
+
+  factory OptsExtraHttp.fromJson(Map<String, dynamic> json) =>
+      _$OptsExtraHttpFromJson(json);
+
+  Map<String, dynamic> toJson() => _$OptsExtraHttpToJson(this);
+}
